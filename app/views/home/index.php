1cea829fc3937370ae44590bb831807cfdfe1e4e
alima <alima@globalnexo.com>
2018-02-20 14:03:08 -0400

    added 2 database

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -21,7 +21,7 @@
 	<h4 class="subtitle">Registrate para que pueda enviarte Tips</h4>
 </div>
 
-<div class="main-container-iframe">
+<div class="main-container-iframe suscribete" style="margin-bottom: 2em;">
 	<form>
 		<div class="form-group">
 			<label for="exampleInputEmail1">Correo electronico</label>
@@ -31,8 +31,39 @@
 			<label for="exampleInputEmail1">Nombre</label>
 			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Ingresa tu nombre">
 		</div>
-		<button type="submit" style="width: 50%; margin: 0;" class="btn btn-success">Registrar</button>
+		<div class="text-center">
+			<button type="submit" style="margin: 0;font-size: 20px;" class="btn btn-success">Suscribete</button>
+		</div>
 	</form>
+</div>
+
+<div class="container mg-t1e2">
+	<h4 class="subtitle">Como montar un Negocio en Internet</h4>
+</div>
+
+<div class="container">
+	<div style="font-size: 17px; font-weight: 700;" class="contein-information">
+		<div>Al igual que tu quizas yo estaba buscando alguna manera de montar un negocio en internet.</div>
+
+		<div>A mi me parecía imposible en un principio y creía que se necesitaba tener mucho conocimiento. </div>
+
+		<div>Entonces lo que hice fue buscar en internet la manera de montar un negocio "ONLINE" y encontre este curso del <a target="_blank" href="http://ofertas777.com/2990/17120">Sr. Eric Collin</a> sobre las maneras que existen para que cualquier persona tenga un Negocio en Internet.</div>
+
+		<div>El curso tiene un costo de 10$ dolares, yo compre el curso y creo que fue la mejor inversion que hice. </div>
+
+		<div>Sinceramente me cambió la manera de pensar, yo antes no hubiese comprado ningún curso en línea lo hubiese buscado por mi cuenta. </div>
+
+		<div>Pero al pensar asi se corre el riesgo de aprender lo mismo en un tiempo más largo y de aprender conceptos equivocados.</div>
+
+		<div>Vale la pena creo yo invertir 10$ en algo que puede cambiar tus ingresos mensuales.</div>
+
+		<div>Yo te puede enseñar todo lo que aprendí y como poner en práctica todo lo aprendido, por ejemplo como crear una pagina web sencilla como esta que estas viendo, o cómo hacer pagos por internet si no sabes como hacerlo, debajo tengo mas videos de lo que aprendí en el proceso de vivir de internet que te pueden ayudar.</div>
+	</div>
+	<button class="center-block" style="margin-top:3em;margin-bottom:3em">
+		<a 	class="btn btn-primary" 
+			style="font-size: 22px; font-weight: 700;" 
+			target="_blank" href="http://ofertas777.com/2990/17120">VER MAS DETALLES SOBRE EL CURSO + BONOS</a>
+	</button>
 </div>
 
 <div class="container">
@@ -67,14 +98,28 @@
 	</div>
 </div>
 
-<div class="social">
+<!-- <div class="social">
 	<ul style="padding: 0;">
 		<li><a href="#" target="_blank" class="icon-facebook"><i class="fab fa-facebook-f"></i></a></li>
 		<li><a href="#" target="_blank" class="icon-twitter"><i class="fab fa-twitter"></i></a></li>
 		<li><a href="#" target="_blank" class="icon-googleplus"><i class="fab fa-google-plus-g"></i></a></li>
-		<!-- <li><a href="#" target="_blank" class="icon-pinterest"></a></li> -->
 		<li><a href="#" class="icon-mail"><i class="fas fa-envelope"></i></a></li>
 	</ul>
+</div> -->
+<br>
+<br>
+<div class="container">
+	<div class="social-clasic text-right">
+		<a 	href="https://www.facebook.com/Trabaja-una-vez-339596089889739/?modal=admin_todo_tour" 
+			target="_blank" 
+			style="width: 111px" 
+			class="icon-facebook">
+			<i class="fab fa-facebook-f"><span style="font-family: arial">ACEBOOK</span></i>
+		</a>
+		<a href="#" target="_blank" class="icon-twitter"><i class="fab fa-twitter"></i></a>
+		<a href="#" target="_blank" class="icon-googleplus"><i class="fab fa-google-plus-g"></i></a>
+		<a href="#" target="_blank" class="icon-mail"><i class="fas fa-envelope"></i></a>
+	</div>
 </div>
 
 <br>
